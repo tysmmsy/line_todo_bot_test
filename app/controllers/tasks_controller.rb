e34c14bf07e682fea071dd7442b00e9ac64642f0
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-09-29 19:31:17 +0900

    コントローラーの編集

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,16 @@
 class TasksController < ApplicationController
   def index
-  @tasks = Task.all
+    @tasks = Task.all
+  end
+
+  def delete
+    task = Task.find(params[:id])
+    if task.destroy
+      redirect_to tasks_index_path,
+        notice:"タスクを一件削除しました"
+    else
+      redirect_to tasks_index_path,
+        alert:"タスクの削除に失敗しました"
+    end
   end
 end
