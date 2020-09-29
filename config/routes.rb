7cbcd68edc8265bb4b032939f91e8b3bee7c05ae
tysmmsy <68370113+tysmmsy@users.noreply.github.com>
2020-09-29 19:34:23 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get 'tasks/index'
+  get 'tasks/:id/edit' => 'tasks#edit',
+      as: "task_edit"
   delete 'tasks/:id/delete' => 'tasks#delete',
-  as: "task_delete"
+    as: "task_delete"
   post 'line/bot'
 end
